f021bd464525fcae4e7570cbb7a7882ccd518713
Pavan <pavantejakusunuri@gmail.com>
2020-09-15 08:52:55 +0530

    Added routes folder for managing routes in project

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
+// Route files
+const users = require("./routes/user");
+
 // Load env Vars
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
+//  Mount routers
+app.use("/api/v1/users", users);
+
 // Body parser
 app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
   PORT,
